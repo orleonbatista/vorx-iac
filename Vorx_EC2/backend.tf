ef3f4a729247e40d858a2c294b364f0704a19c76
Orleon Batista <orleonbatista@gmail.com>
2021-11-04 21:43:01 -0300

    update files

--- Vorx_EC2/backend.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "vorx-iac-orleon"
-    key    = "ec2-terraform.tfstate"
-    region = "us-east-1"
-  }
-}
--- Vorx_EC2/variables.tf
+++
@@ -1,3 +0,0 @@
-variable "vpc_id" {
-  default = "vpc-0d6f49dabd69706bd"
-}
